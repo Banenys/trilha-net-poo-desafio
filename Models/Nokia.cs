f34d1feda672e6046e3a5a846dfac25c222129d7
Eny Banenys <145375629+Banenys@users.noreply.github.com>
2024-01-07 15:04:37 -0300

    Update Nokia.cs

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,19 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    // Herdar da classe "Smartphone"
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        // Construtor para a classe Nokia
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+            // Qualquer inicialização específica para a classe Nokia pode ser feita aqui
+        }
+
+        // Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} em um Nokia.");
+            // Lógica específica de instalação para Nokia
+        }
     }
 }
