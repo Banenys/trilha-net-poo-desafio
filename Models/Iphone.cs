3e6ba2363f6b629cfe846978bd984a63e4805e32
Eny Banenys <145375629+Banenys@users.noreply.github.com>
2024-01-07 15:05:14 -0300

    Update Iphone.cs

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,19 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    // Herdar da classe "Smartphone"
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        // Construtor para a classe Iphone
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+            // Qualquer inicialização específica para a classe Iphone pode ser feita aqui
+        }
+
+        // Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} em um iPhone.");
+            // Lógica específica de instalação para iPhone
+        }
     }
 }
